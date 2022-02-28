cae0152994475aba603e0320ac82e1fe0c687a6a
Sonic853 <Sonic853@qq.com>
2022-02-28 20:16:06 +0800

    Merge page

--- html/bundle.js
+++ html/bundle.js
@@ -22,9 +22,7 @@
     return target;
   };
   var __toModule = (module) => {
-    if (module && module.__esModule)
-      return module;
-    return __exportStar(__markAsModule(__defProp(module != null ? __create(__getProtoOf(module)) : {}, "default", {value: module, enumerable: true})), module);
+    return __exportStar(__markAsModule(__defProp(module != null ? __create(__getProtoOf(module)) : {}, "default", module && module.__esModule && "default" in module ? {get: () => module.default, enumerable: true} : {value: module, enumerable: true})), module);
   };
 
   // node_modules/apexcharts/dist/apexcharts.common.js
@@ -7740,21 +7738,14 @@
     user_settings: "00000008-0000-3512-2118-0009af100700",
     music_notification: "00000010-0000-3512-2118-0009af100700",
     deviceevent: "00000010-0000-3512-2118-0009af100700",
-    chunked_transfer: "00000020-0000-3512-2118-0009af100700"
+    chunked_transfer: "00000020-0000-3512-2118-0009af100700",
+    chunked_transfer_2021_write: "00000016-0000-3512-2118-0009af100700",
+    chunked_transfer_2021_read: "00000017-0000-3512-2118-0009af100700"
   };
   var ADVERTISEMENT_SERVICE = 65248;
 
   // src/miband.js
-  function buf2hex(buffer) {
-    return Array.prototype.map.call(new Uint8Array(buffer), (x) => ("00" + x.toString(16)).slice(-2)).join("");
-  }
-  var concatBuffers = (buffer1, buffer2) => {
-    const out = new Uint8Array(buffer1.byteLength + buffer2.byteLength);
-    out.set(new Uint8Array(buffer1), 0);
-    out.set(new Uint8Array(buffer2), buffer1.byteLength);
-    return out.buffer;
-  };
-  var MiBand5 = class {
+  var MiBand = class {
     constructor(authKey) {
       if (!authKey.match(/^[a-zA-Z0-9]{32}$/)) {
         throw new Error("Invalid auth key, must be 32 hex characters such as '94359d5b8b092e1286a43cfb62ee7923'");
@@ -7762,55 +7753,6 @@
       this.authKey = authKey;
       this.services = {};
       this.chars = {};
-    }
-    async init() {
-      const device = await navigator.bluetooth.requestDevice({
-        filters: [
-          {
-            services: [ADVERTISEMENT_SERVICE]
-          }
-        ],
-        optionalServices: [UUIDS.miband2, UUIDS.heartrate, UUIDS.miband1]
-      });
-      window.dispatchEvent(new CustomEvent("connected"));
-      await device.gatt.disconnect();
-      const server = await device.gatt.connect();
-      console.log("Connected through gatt");
-      this.services.miband1 = await server.getPrimaryService(UUIDS.miband1);
-      this.services.miband2 = await server.getPrimaryService(UUIDS.miband2);
-      this.services.heartrate = await server.getPrimaryService(UUIDS.heartrate);
-      console.log("Services initialized");
-      this.chars.auth = await this.services.miband2.getCharacteristic(CHAR_UUIDS.auth);
-      this.chars.hrControl = await this.services.heartrate.getCharacteristic(CHAR_UUIDS.heartrate_control);
-      this.chars.hrMeasure = await this.services.heartrate.getCharacteristic(CHAR_UUIDS.heartrate_measure);
-      this.chars.sensor = await this.services.miband1.getCharacteristic(CHAR_UUIDS.sensor);
-      console.log("Characteristics initialized");
-      await this.authenticate();
-    }
-    async authenticate() {
-      await this.startNotifications(this.chars.auth, async (e) => {
-        const value = e.target.value.buffer;
-        const cmd = buf2hex(value.slice(0, 3));
-        if (cmd === "100101") {
-          console.log("Set new key OK");
-        } else if (cmd === "100201") {
-          const number = value.slice(3);
-          console.log("Received authentication challenge: ", buf2hex(value.slice(3)));
-          const key = aesjs.utils.hex.toBytes(this.authKey);
-          const aesCbc = new aesjs.ModeOfOperation.cbc(key);
-          const out = aesCbc.encrypt(new Uint8Array(number));
-          const cmd2 = concatBuffers(new Uint8Array([3, 0]), out);
-          console.log("Sending authentication response");
-          await this.chars.auth.writeValue(cmd2);
-        } else if (cmd === "100301") {
-          await this.onAuthenticated();
-        } else if (cmd === "100308") {
-          console.log("Received authentication failure");
-        } else {
-          throw new Error(`Unknown callback, cmd='${cmd}'`);
-        }
-      });
-      await this.chars.auth.writeValue(Uint8Array.from([2, 0]));
     }
     async onAuthenticated() {
       console.log("Authentication successful");
@@ -7839,7 +7781,225 @@
       char.addEventListener("characteristicvaluechanged", cb);
     }
   };
+  var MiBand6 = class extends MiBand {
+    constructor(authKey) {
+      super(authKey);
+      this.handle = 0;
+      this.reassembleBuffer = new Uint8Array(512);
+      this.lastSequenceNumber = 0;
+      this.reassembleBuffer_pointer = 0;
+      this.reassembleBuffer_expectedBytes = 0;
+      this.prv_buf = null;
+      this.pub_buf = null;
+      this.sec_buf = null;
+      this.prv = null;
+      this.pub = null;
+      this.sec = null;
+    }
+    getNextHandle() {
+      return this.handle++;
+    }
+    async init() {
+      const device = await navigator.bluetooth.requestDevice({
+        filters: [
+          {
+            services: [ADVERTISEMENT_SERVICE]
+          }
+        ],
+        optionalServices: [UUIDS.miband2, UUIDS.heartrate, UUIDS.miband1]
+      });
+      window.dispatchEvent(new CustomEvent("connected"));
+      await device.gatt.disconnect();
+      const server = await device.gatt.connect();
+      console.log("Connected through gatt");
+      this.services.miband1 = await server.getPrimaryService(UUIDS.miband1);
+      this.services.miband2 = await server.getPrimaryService(UUIDS.miband2);
+      this.services.heartrate = await server.getPrimaryService(UUIDS.heartrate);
+      console.log("Services initialized");
+      this.chars.auth = await this.services.miband2.getCharacteristic(CHAR_UUIDS.auth);
+      this.chars.chunkedWrite = await this.services.miband1.getCharacteristic(CHAR_UUIDS.chunked_transfer_2021_write);
+      this.chars.chunkedRead = await this.services.miband1.getCharacteristic(CHAR_UUIDS.chunked_transfer_2021_read);
+      this.chars.hrControl = await this.services.heartrate.getCharacteristic(CHAR_UUIDS.heartrate_control);
+      this.chars.hrMeasure = await this.services.heartrate.getCharacteristic(CHAR_UUIDS.heartrate_measure);
+      this.chars.sensor = await this.services.miband1.getCharacteristic(CHAR_UUIDS.sensor);
+      console.log("Characteristics initialized");
+      await this.authenticate();
+    }
+    async authenticate() {
+      const CHUNKED2021_ENDPOINT_AUTH = 130;
+      await this.startNotifications(this.chars.chunkedRead, async (e) => {
+        const value = new Uint8Array(e.target.value.buffer);
+        if (value.length > 1 && value[0] == 3) {
+          const sequenceNumber = value[4];
+          let headerSize;
+          if (sequenceNumber == 0 && value[9] == CHUNKED2021_ENDPOINT_AUTH && value[10] == 0 && value[11] == 16 && value[12] == 4 && value[13] == 1) {
+            console.log("A");
+            this.reassembleBuffer_pointer = 0;
+            headerSize = 14;
+            this.reassembleBuffer_expectedBytes = value[5] - 3;
+          } else if (sequenceNumber > 0) {
+            if (sequenceNumber != this.lastSequenceNumber + 1) {
+              console.log("Unexpected sequence number");
+              return;
+            }
+            headerSize = 5;
+          } else if (value[9] == CHUNKED2021_ENDPOINT_AUTH && value[10] == 0 && value[11] == 16 && value[12] == 5 && value[13] == 1) {
+            console.log("Successfully authenticated");
+            await this.onAuthenticated();
+            return true;
+          } else {
+            console.log("Unhandled characteristic change");
+            return false;
+          }
+          const bytesToCopy = value.length - headerSize;
+          this.reassembleBuffer.set(new Uint8Array(value).subarray(headerSize), this.reassembleBuffer_pointer);
+          this.reassembleBuffer_pointer += bytesToCopy;
+          this.lastSequenceNumber = sequenceNumber;
+          if (this.reassembleBuffer_pointer == this.reassembleBuffer_expectedBytes) {
+            const remoteRandom = new Uint8Array(this.reassembleBuffer.subarray(0, 16));
+            const remotePublicEC = new Uint8Array(this.reassembleBuffer.subarray(16, 64));
+            const rpub_buf = Module._malloc(ECC_PUB_KEY_SIZE);
+            const rpub = Module.HEAPU8.subarray(rpub_buf, rpub_buf + ECC_PUB_KEY_SIZE);
+            rpub.set(remotePublicEC);
+            const sec_buf = Module._malloc(ECC_PUB_KEY_SIZE);
+            const sec = Module.HEAPU8.subarray(sec_buf, sec_buf + ECC_PUB_KEY_SIZE);
+            Module._ecdh_shared_secret(this.prv_buf, rpub_buf, sec_buf);
+            this.encryptedSequenceNr = sec[0] & 255 | (sec[1] & 255) << 8 | (sec[2] & 255) << 16 | (sec[3] & 255) << 24;
+            const secretKey = aesjs.utils.hex.toBytes(this.authKey);
+            const finalSharedSessionAES = new Uint8Array(16);
+            for (let i = 0; i < 16; i++) {
+              finalSharedSessionAES[i] = sec[i + 8] ^ secretKey[i];
+            }
+            this.sharedSessionKey = finalSharedSessionAES;
+            const aesCbc1 = new aesjs.ModeOfOperation.cbc(secretKey);
+            const out1 = aesCbc1.encrypt(remoteRandom);
+            const aesCbc2 = new aesjs.ModeOfOperation.cbc(finalSharedSessionAES);
+            const out2 = aesCbc2.encrypt(remoteRandom);
+            if (out1.length == 16 && out2.length == 16) {
+              const command = new Uint8Array(33);
+              command[0] = 5;
+              command.set(out1, 1);
+              command.set(out2, 17);
+              console.log("Sending 2nd auth part");
+              await this.writeChunkedValue(this.chars.chunkedWrite, CHUNKED2021_ENDPOINT_AUTH, this.getNextHandle(), command);
+            }
+          }
+          return true;
+        }
+      });
+      const ECC_PUB_KEY_SIZE = 48;
+      const ECC_PRV_KEY_SIZE = 24;
+      this.pub_buf = Module._malloc(ECC_PUB_KEY_SIZE);
+      this.prv_buf = Module._malloc(ECC_PRV_KEY_SIZE);
+      this.pub = Module.HEAPU8.subarray(this.pub_buf, this.pub_buf + ECC_PUB_KEY_SIZE);
+      this.prv = Module.HEAPU8.subarray(this.prv_buf, this.prv_buf + ECC_PRV_KEY_SIZE);
+      crypto.getRandomValues(this.prv);
+      Module._ecdh_generate_keys(this.pub_buf, this.prv_buf);
+      const auth = this.getInitialAuthCommand(this.pub);
+      console.log("Sending first auth");
+      await this.writeChunkedValue(this.chars.chunkedWrite, CHUNKED2021_ENDPOINT_AUTH, this.getNextHandle(), Uint8Array.from(auth));
+    }
+    getInitialAuthCommand(publicKey) {
+      return [4, 2, 0, 2, ...publicKey];
+    }
+    async writeChunkedValue(char, type, handle, data) {
+      let remaining = data.length;
+      let count = 0;
+      let header_size = 11;
+      const mMTU = 23;
+      while (remaining > 0) {
+        const MAX_CHUNKLENGTH = mMTU - 3 - header_size;
+        const copybytes = Math.min(remaining, MAX_CHUNKLENGTH);
+        const chunk = new Uint8Array(copybytes + header_size);
+        let flags = 0;
+        if (count == 0) {
+          flags |= 1;
+          let i = 5;
+          chunk[i++] = data.length & 255;
+          chunk[i++] = data.length >> 8 & 255;
+          chunk[i++] = data.length >> 16 & 255;
+          chunk[i++] = data.length >> 24 & 255;
+          chunk[i++] = type & 255;
+          chunk[i] = type >> 8 & 255;
+        }
+        if (remaining <= MAX_CHUNKLENGTH) {
+          flags |= 6;
+        }
+        chunk[0] = 3;
+        chunk[1] = flags;
+        chunk[2] = 0;
+        chunk[3] = handle;
+        chunk[4] = count;
+        chunk.set(data.slice(data.length - remaining, data.length - remaining + copybytes), header_size);
+        await char.writeValue(chunk);
+        remaining -= copybytes;
+        header_size = 5;
+        count++;
+      }
+    }
+  };
+  var MiBand5 = class extends MiBand {
+    async init() {
+      const device = await navigator.bluetooth.requestDevice({
+        filters: [
+          {
+            services: [ADVERTISEMENT_SERVICE]
+          }
+        ],
+        optionalServices: [UUIDS.miband2, UUIDS.heartrate, UUIDS.miband1]
+      });
+      window.dispatchEvent(new CustomEvent("connected"));
+      await device.gatt.disconnect();
+      const server = await device.gatt.connect();
+      console.log("Connected through gatt");
+      this.services.miband1 = await server.getPrimaryService(UUIDS.miband1);
+      this.services.miband2 = await server.getPrimaryService(UUIDS.miband2);
+      this.services.heartrate = await server.getPrimaryService(UUIDS.heartrate);
+      console.log("Services initialized");
+      this.chars.auth = await this.services.miband2.getCharacteristic(CHAR_UUIDS.auth);
+      this.chars.hrControl = await this.services.heartrate.getCharacteristic(CHAR_UUIDS.heartrate_control);
+      this.chars.hrMeasure = await this.services.heartrate.getCharacteristic(CHAR_UUIDS.heartrate_measure);
+      this.chars.sensor = await this.services.miband1.getCharacteristic(CHAR_UUIDS.sensor);
+      console.log("Characteristics initialized");
+      await this.authenticate();
+    }
+    buf2hex(buffer) {
+      return Array.prototype.map.call(new Uint8Array(buffer), (x) => ("00" + x.toString(16)).slice(-2)).join("");
+    }
+    concatBuffers(buffer1, buffer2) {
+      const out = new Uint8Array(buffer1.byteLength + buffer2.byteLength);
+      out.set(new Uint8Array(buffer1), 0);
+      out.set(new Uint8Array(buffer2), buffer1.byteLength);
+      return out.buffer;
+    }
+    async authenticate() {
+      await this.startNotifications(this.chars.auth, async (e) => {
+        const value = e.target.value.buffer;
+        const cmd = this.buf2hex(value.slice(0, 3));
+        if (cmd === "100101") {
+          console.log("Set new key OK");
+        } else if (cmd === "100201") {
+          const number = value.slice(3);
+          console.log("Received authentication challenge: ", this.buf2hex(value.slice(3)));
+          const key = aesjs.utils.hex.toBytes(this.authKey);
+          const aesCbc = new aesjs.ModeOfOperation.cbc(key);
+          const out = aesCbc.encrypt(new Uint8Array(number));
+          const cmd2 = this.concatBuffers(new Uint8Array([3, 0]), out);
+          console.log("Sending authentication response");
+          await this.chars.auth.writeValue(cmd2);
+        } else if (cmd === "100301") {
+          await this.onAuthenticated();
+        } else if (cmd === "100308") {
+          console.log("Received authentication failure");
+        } else {
+          throw new Error(`Unknown callback, cmd='${cmd}'`);
+        }
+      });
+      await this.chars.auth.writeValue(Uint8Array.from([2, 0]));
+    }
+  };
   window.MiBand5 = MiBand5;
+  window.MiBand6 = MiBand6;
 
   // src/chart.js
   var import_apexcharts = __toModule(require_apexcharts_common());
