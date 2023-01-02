0ad8d23039b429cbc8f183dfddb507edf749f925
Runnin4ik <mr.kirilll12@mail.ru>
2023-01-02 21:34:24 +0300

    add send HR to chatbox

--- app.js
+++ app.js
@@ -10,44 +10,71 @@ let vrchatOSC = new osc.UDPPort({
     metadata: true
 });
 
+let sendToChatbox = "false";
+let chatboxText = "❤{HR} bpm";
+
 vrchatOSC.open();
-open('https://vard88508.github.io/vrc-osc-miband-hrm/html/');
+// open('https://vard88508.github.io/vrc-osc-miband-hrm/html/');
 console.log("Waiting for connection from browser...");
 
 server.on('connection', ws => {
     console.log("Connected. Waiting for data...");
     ws.on('message', function message(data) {
-        if (data == 0) {
-            console.log("Got heart rate: 0 bpm, skipping parameter update...");
+        let chatbox_text = JSON.parse(data).text;
+        let data_string = data.toString();
+        if(chatbox_text) {
+            chatboxText = chatbox_text;
+        } else if(data_string === "true" || data_string === "false") {
+            sendToChatbox = data_string;
         } else {
-            console.log('Got heart rate: %s bpm', data);
-            let heartrate = {
-                address: "/avatar/parameters/Heartrate",
-                args:
-                    {
-                        type: "f",
-                        value: data/127-1
-                    }
-            };
-            let heartrate2 = {
-                address: "/avatar/parameters/Heartrate2",
-                args:
-                    {
-                        type: "f",
-                        value: data/255
-                    }
-            };
-            let heartrate3 = {
-                address: "/avatar/parameters/Heartrate3",
-                args:
-                    {
-                        type: "i",
-                        value: data
-                    }
-            };
-            vrchatOSC.send(heartrate);
-            vrchatOSC.send(heartrate2);
-            vrchatOSC.send(heartrate3);
+            if (data === "0") {
+                console.log("Got heart rate: 0 bpm, skipping parameter update...");
+            } else {
+                console.log('Got heart rate: %s bpm', data);
+                let heartrate = {
+                    address: "/avatar/parameters/Heartrate",
+                    args:
+                        {
+                            type: "f",
+                            value: data/127-1
+                        }
+                };
+                let heartrate2 = {
+                    address: "/avatar/parameters/Heartrate2",
+                    args:
+                        {
+                            type: "f",
+                            value: data/255
+                        }
+                };
+                let heartrate3 = {
+                    address: "/avatar/parameters/Heartrate3",
+                    args:
+                        {
+                            type: "i",
+                            value: data
+                        }
+                };
+                vrchatOSC.send(heartrate);
+                vrchatOSC.send(heartrate2);
+                vrchatOSC.send(heartrate3);
+
+                if(sendToChatbox === "true") {
+
+                    let text = chatboxText.replace("{HR}", data_string)+"    "
+                    // console.log('send '+ text);
+
+                    let heartrate_chatbox = {
+                        address: "/chatbox/input",
+                        args: [
+                            { type: "s", value: text},
+                            { type: "T", value: true}
+                        ],
+                    };
+
+                    vrchatOSC.send(heartrate_chatbox);
+                }
+            }
         }
     });
 });
